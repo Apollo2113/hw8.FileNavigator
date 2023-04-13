4bd4ba595c334d09185fdf2ec0d4871bbabb168b
Apollo2113 <petrenkoroman1@gmail.com>
2023-04-13 21:43:03 +0300

    added  method add

--- src/FileNavigator.java
+++ src/FileNavigator.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FileNavigator {
+
+    private final Map<String, List<FileData>> directories = new HashMap<>();
+    public void add(FileData file) {
+        directories.computeIfAbsent(file.getPath(), k -> new ArrayList<>()).add(file);
+    }
+
 }
